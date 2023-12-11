eb7e9783c2c2f3b2864069e5131495f63dfaa3b5
Jett <jett@moego.pet>
2023-12-11 15:40:13 +0800

    feat: add mock api

--- achobeta-official-service/src/main/java/com/achobeta/official/service/controller/MockController.java
+++ achobeta-official-service/src/main/java/com/achobeta/official/service/controller/MockController.java
@@ -2,6 +2,8 @@ package com.achobeta.official.service.controller;
 
 
 import com.achobeta.official.service.common.SystemJsonResponse;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,10 @@ public class MockController {
     @PostMapping("/start")
     public SystemJsonResponse start() {
         return SystemJsonResponse.SYSTEM_SUCCESS("mock docker success !");
+    }
+
+    @GetMapping("/start")
+    public  SystemJsonResponse startGet() {
+        return SystemJsonResponse.SYSTEM_SUCCESS("mock get success !");
     }
 }
